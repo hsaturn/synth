3ffc3d3fee33c3873e377c26ac2ee036971c5369
hsaturn <hsaturn@gmail.com>
2021-05-23 18:28:04 +0200

    Better dtmf.sh script

--- tests/dtmf.sh
+++ tests/dtmf.sh
@@ -1,64 +1,47 @@
+if [ "$*" == "" ]; then
+	echo "Usage: $0 numbers"
+	echo
+	echo "    Allowed numbers: 0123456789ABCD#"
+	echo "    -ms time changes the length of numbers"
+	echo
+fi
+ms=200
 while [ "$1" != "" ]; do
-	if [ "$1" == "1" ]; then
-		f1=1209
-		f2=697
+	if [ "$1" == "-t" ]; then
+		shift
+		ms=$1
+	else
+		numbers="$1"
+		while [ "$numbers" != "" ]; do
+			number=${numbers:0:1}
+			numbers=${numbers:1}
+			sound=1
+			case $number in
+				1) f1=1209; f2=697;;
+				2) f1=1336; f2=697;;
+				3) f1=1477; f2=697;;
+				A) f1=11633; f2=697;;
+				4) f1=1209; f2=770;;
+				5) f1=1336; f2=770;;
+				6) f1=1477; f2=770;;
+				B) f1=1633; f2=770;;
+				7) f1=1209; f2=852;;
+				8) f1=1336; f2=852;;
+				9) f1=1477; f2=852;;
+				C) f1=1633; f2=852;;
+				0) f1=1336; f2=941;;
+				[#]) f1=1477; f2=941;;
+				D) f1=1633; f2=941;;
+				' ') sleep 0.2; sound=0; echo -n ' ';;
+				*) sound=0; echo "($number : ???)"
+			esac
+			if [ "$sound" == "1" ]; then
+				echo -n "$number"
+				synth $ms sinus $f1:50 sinus $f2:50
+			fi
+	  done
 	fi
-	if [ "$1" == "2" ]; then
-		f1=1336
-		f2=697
-	fi
-	if [ "$1" == "3" ]; then
-		f1=1477
-		f2=697
-	fi
-	if [ "$1" == "A" ]; then
-		f1=11633
-		f2=697
-	fi
-	if [ "$1" == "4" ]; then
-		f1=1209
-		f2=770
-	fi
-	if [ "$1" == "5" ]; then
-		f1=1336
-		f2=770
-	fi
-	if [ "$1" == "6" ]; then
-		f1=1477
-		f2=770
-	fi
-	if [ "$1" == "B" ]; then
-		f1=1633
-		f2=770
-	fi
-	if [ "$1" == "7" ]; then
-		f1=1209
-		f2=852
-	fi
-	if [ "$1" == "8" ]; then
-		f1=1336
-		f2=852
-	fi
-	if [ "$1" == "9" ]; then
-		f1=1477
-		f2=852
-	fi
-	if [ "$1" == "C" ]; then
-		f1=1633
-		f2=852
-	fi
-	if [ "$1" == "0" ]; then
-		f1=1336
-		f2=941
-	fi
-	if [ "$1" == "#" ]; then
-		f1=1477
-		f2=941
-	fi
-	if [ "$1" == "D" ]; then
-		f1=1633
-		f2=941
-	fi
-	synth 200 sinus $f1:50 sinus $f2:50
 	shift
 done
+
+echo
