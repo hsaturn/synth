3872f66c17d089026370bbae4ad0c4f699f725e9
hsaturn <hsaturn@gmail.com>
2021-05-24 23:10:09 +0200

    Chain example added in vrac

--- tests/vrac.sh
+++ tests/vrac.sh
@@ -4,3 +4,4 @@ synth 5000 fm 80 120 sin 440 fm 50 150 sq 1337 sq 10
 synth 5000 am 0 150 fm 0 440 sinus 110:50 sinus 33 sinus 10
 synth 3000 am 0 100 sinus 1 distorsion 100 sinus 500  sinus 220
 
+synth am 50 150 sinus 1 sinus 100 chain ms 15 gen triangle DO RE MI FA SOL FA MI RE mix 10 loop
