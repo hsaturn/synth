c32ae77d2ab12d9af3ab2837c093d04f648b623e
hsaturn <hsaturn@gmail.com>
2021-05-24 06:09:13 +0200

    Modif vrac examples

--- tests/vrac.sh
+++ tests/vrac.sh
@@ -1,6 +1,6 @@
 synth fm 0 120 sinus 440 adsr 1:0 1000:100 2000:0 loop sinus 440
 synth reverb 60:92 fm 80 120 sin 440:10 sq 1
-synth fm 80 120 sin 440 fm 50 150 sq 1337 sq 10
-synth am 0 150 fm 0 440 sinus 110:50 sinus 33 sinus 10
-synth 1000 am 0 100 sinus 1 distorsion 100 sinus 500  sinus 220
+synth 5000 fm 80 120 sin 440 fm 50 150 sq 1337 sq 10
+synth 5000 am 0 150 fm 0 440 sinus 110:50 sinus 33 sinus 10
+synth 3000 am 0 100 sinus 1 distorsion 100 sinus 500  sinus 220
 
